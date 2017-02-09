4d9fe3d2fdfd9aa916be63900ae808bff370e691
George Shuklin <george.shuklin@gmail.com>
2017-02-09 02:02:56 +0200

    Some nice work on prive_key_file in SSH class.
    No we no longer will 'save' key in OSClient. We will pass
    content of the key to SSH class and it will handle all things with
    writing it out, cleaning.
    
    I'll use same approach for ssh config.

--- dibctl/ssh.py
+++ dibctl/ssh.py
@@ -1,12 +1,19 @@
-'''all stuff for ssh manipulation'''
+import tempfile
 
 
 class SSH(object):
-    def __init__(self, ip, username, path_to_private_key, port=22):
+    def __init__(self, ip, username, private_key, port=22):
         self.ip = ip
         self.username = username
-        self.path_to_private_key = path_to_private_key
+        self.private_key = private_key
         self.port = port
+        self.private_key_file = None
+
+    def save_private_key(self):
+        pass
+
+    def wipe_private_key(self):
+        pass
 
     def user_host_and_port(self):
         if self.port == 22:
@@ -18,6 +25,15 @@ class SSH(object):
                 port=str(self.port)
             )
 
+    def key_file(self):
+        if not self.private_key_file:
+            self.private_key_file = tempfile.NamedTemporaryFile(
+                prefix='dibctl_key_',
+            )
+            self.private_key_file.write(self.private_key)
+            self.private_key_file.flush()
+        return self.private_key_file.name
+
     def command_line(self):
         command_line = [
             "ssh",
@@ -25,6 +41,6 @@ class SSH(object):
             "-o", "UserKnownHostsFile=/dev/null",
             "-o", "UpdateHostKeys=no",
             "-o", "PasswordAuthentication=no",
-            "-i", self.path_to_private_key,
+            "-i", self.key_file(),
             self.user_host_and_port()
         ]
--- tests/test_ssh.py
+++ tests/test_ssh.py
@@ -13,13 +13,41 @@ def ssh():
     return ssh
 
 
-@pytest.mark.parametrize('host, port, user, output', [
+@pytest.mark.parametrize('ip, port, user, output', [
     ['192.168.1.1', 22, 'user', 'user@192.168.1.1'],
     ['192.168.1.1', 1222, 'user', 'user@192.168.1.1:1222']
 ])
-def test_user_host_and_port(ssh, host, port, user, output):
-    s = ssh.SSH(host, user, None, port)
+def test_user_host_and_port(ssh, ip, port, user, output):
+    s = ssh.SSH(ip, user, None, port)
     assert s.user_host_and_port() == output
+
+
+def test_key_file(ssh):
+    with mock.patch.object(ssh.tempfile, 'NamedTemporaryFile') as mock_tmp:
+        mock_tmp.return_value.name = sentinel.filename
+        s = ssh.SSH(sentinel.ip, sentinel.user, "secret", sentinel.port)
+        assert s.key_file() == sentinel.filename
+        assert mock_tmp.return_value.write.call_args == mock.call('secret')
+
+
+def test_key_file_name(ssh):
+    s = ssh.SSH(sentinel.ip, sentinel.user, "secret", sentinel.port)
+    f = s.key_file()
+    assert 'dibctl_key' in f
+
+
+def test_key_file_content(ssh):
+    s = ssh.SSH(sentinel.ip, sentinel.user, "secret", sentinel.port)
+    f = s.key_file()
+    assert open(f, 'r').read() == "secret"
+
+
+def test_key_file_remove_afterwards(ssh):
+    s = ssh.SSH(sentinel.ip, sentinel.user, "secret", sentinel.port)
+    f = s.key_file()
+    del s
+    with pytest.raises(IOError):
+        open(f, 'r')
 
 
 if __name__ == "__main__":
