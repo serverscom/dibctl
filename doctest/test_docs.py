6d6ce90974b9ea51de103b35f99c788b0bb34f7b
George Shuklin <george.shuklin@gmail.com>
2017-02-28 11:23:49 +0200

    Add dibctl validate call as part of doctest

--- doctest/test_docs.py
+++ doctest/test_docs.py
@@ -3,6 +3,7 @@ import os
 import inspect
 import sys
 import pytest
+import mock
 
 
 ourfilename = os.path.abspath(inspect.getfile(inspect.currentframe()))
@@ -15,6 +16,13 @@ def config():
     from dibctl import config
     return config
 
+
+@pytest.fixture
+def commands():
+    from dibctl import commands
+    return commands
+
+
 # this is integration test
 # it uses 'docs/example_configs/upload.yaml' file to
 # ensure that examples and code stay in sync
@@ -22,17 +30,32 @@ def config():
 # it mainly checks the schema but also all other
 # bits related to 'forced config'. No mocks involved
 
+images_yaml = os.path.join(parentdir, "docs/example_configs/images.yaml")
+test_yaml = os.path.join(parentdir, "docs/example_configs/test.yaml")
+upload_yaml = os.path.join(parentdir, "docs/example_configs/upload.yaml")
+
 
 def test_integration_uploadenvconfig_schema_from_docs_example(config):
-    config.UploadEnvConfig(os.path.join(parentdir, "docs/example_configs/upload.yaml"))
+    config.UploadEnvConfig(upload_yaml)
 
 
 def test_integration_testenvconfig_schema_from_docs_example(config):
-    config.TestEnvConfig(os.path.join(parentdir, "docs/example_configs/test.yaml"))
+    config.TestEnvConfig(test_yaml)
 
 
 def test_integration_imageconfig_schema_from_docs_example(config):
-    config.ImageConfig(os.path.join(parentdir, "docs/example_configs/images.yaml"))
+    config.ImageConfig(images_yaml)
+
+
+def test_call_actual_dibctl_validate(commands):
+    with mock.patch.object(commands.sys, "exit") as mock_exit:
+        commands.main([
+            'validate',
+            '--images-config', images_yaml,
+            '--upload-config', upload_yaml,
+            '--test-config', test_yaml
+        ])
+        assert mock_exit.call_args[0][0] == 0
 
 
 if __name__ == "__main__":
