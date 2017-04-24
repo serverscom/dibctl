21c647b0f959f56d6b130ab2b62a2267bcf52d36
George Shuklin <george.shuklin@gmail.com>
2017-04-24 22:31:02 +0300

    Cleanup bad ENV variables to make tests easier to process

--- integration_tests/test_exit_codes.py
+++ integration_tests/test_exit_codes.py
@@ -5,6 +5,16 @@ import os
 def setup_module(module):
     global curdir
     curdir = os.getcwd()
+    for forbidden in [
+        'OS_AUTH_URL',
+        'OS_USERNAME',
+        'OS_PASSWORD',
+        'OS_TENANT_NAME',
+        'OS_PROJECT_NAME',
+        'OS_PROJECT'
+    ]:
+        if forbidden in os.environ:
+            del os.environ[forbidden]
     if 'integration_tests' not in curdir:
         os.chdir('integration_tests')
 
