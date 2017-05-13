845df7b1f93f1258e79bc6cbf8358df506fd27cf
George Shuklin <george.shuklin@gmail.com>
2017-05-13 06:45:45 +0300

    Refactor timeout. Other code is not ready

--- dibctl/timeout.py
+++ dibctl/timeout.py
@@ -1,13 +1,22 @@
 import signal
 
 
+class TimeoutError(EnvironmentError):
+    pass
+
+
 class timeout(object):
-    def __init__(self, timeout, handler):
+    def __init__(self, timeout):
         self.timeout = timeout
-        self.handler = handler
+
+    def raise_timeout(self, signum, frame):
+        if signum == signal.SIGALRM:
+            raise TimeoutError("Timed out after %s" % self.timeout)
+        else:
+            raise RuntimeError("Signal %s, have no idea what to do" % signum)
 
     def __enter__(self):
-        signal.signal(signal.SIGALRM, self.handler)
+        signal.signal(signal.SIGALRM, self.raise_timeout)
         signal.alarm(self.timeout)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
